0dffdf0ebacb0e15bfb62909dba7db493c28bb6a
misProyectosPropios <inakifrutos@outlook.com>
2024-08-10 20:39:05 -0300

    UPDATE ./client-side/style.css: GET THE BUTTONS

---
+++ client-side/js.js
@@ -0,0 +1,11 @@
+/*
+ * Get the buttons and visor
+ */
+var visor = document.getElementById("calculadora-visor");
+var botones = document.getElementsByClassName("table-boton")
+var numeros = [];
+var pantallaEscrita = 0;
+
+/*
+ * Iterate over every buttons
+ */
